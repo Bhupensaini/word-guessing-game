41de4fbd185f09bdf0f6d4254b8788349293c1ee
Bhupensaini <63636192+Bhupensaini@users.noreply.github.com>
2020-06-10 00:31:50 +0530

    initial commit

---
+++ lib/mymodule.py
@@ -0,0 +1,17 @@
+word_list = {
+    "john denver",
+    "deku",
+    'izuku medoriya',
+    'toshinori',
+    'All might',
+    "salamander",
+    "anime",
+    'Tanjiro kamado',
+    'zenitsu',
+    'tomaioka gyui',
+    'inosuke',
+    'muan kubutsuji',
+    'nezuko',
+    'all might',
+    'nezuko chan'
+}
